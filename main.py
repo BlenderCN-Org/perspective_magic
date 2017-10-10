d00fc4f69e4b0c414482ab16d25e5e9b3083c749
MatsumasaH <matumasa2022@gmail.com>
2017-10-11 02:42:45 +0300

    オートでパースを計算するソフトを追加、すばらしい。一応説明を書いておくと、カメラが0,-8,0に一つ必要で、A,AD,B,BDというエンプティーを二点におけば自動的に色々計算してくれる。詳しい説明はいつか。

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import bpy
 import re
 import bpy_extras
 import bmesh
+import mathutils
 
 # やはり人の作ったものは美しい
 # 完璧なアドオンだ
@@ -71,12 +72,11 @@ def get_screen_position(co):
     obj = bpy.context.scene.camera
     # obj = bpy.context.object
 
-    co = co.location
     # co = bpy.context.active_object.location
     # co = bpy.context.scene.cursor_location
 
     co_2d = bpy_extras.object_utils.world_to_camera_view(scene, obj, co)
-    print("2D Coords:", co_2d)
+    # print("2D Coords:", co_2d)
 
     # If you want pixel coords
     render_scale = scene.render.resolution_percentage / 100
@@ -84,11 +84,11 @@ def get_screen_position(co):
         int(scene.render.resolution_x * render_scale),
         int(scene.render.resolution_y * render_scale),
     )
-    print("Pixel Coords:", (
-        round(co_2d.x * render_size[0]),
-        round(co_2d.y * render_size[1]),
-    ))
-    return (co_2d.x * render_size[0],co_2d.y * render_size[1],0)
+    # print("Pixel Coords:", (
+    #    round(co_2d.x * render_size[0]),
+    #    round(co_2d.y * render_size[1]),
+    #))
+    return mathutils.Vector((co_2d.x * render_size[0],co_2d.y * render_size[1],0))
 
 class PerspectiveMagicModal(bpy.types.Operator):
     bl_idname = "object.perspective_magic_modal_operator"
@@ -210,6 +210,158 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+
+# ----------------------------------------------------------------------------
+# Test Here
+# ----------------------------------------------------------------------------
+
+class Struct:
+    def __init__(self, A, AD, B, BD, center, length, mirror,al,adl):
+        self.A = A
+        self.AD = AD
+        self.B = B
+        self.BD = BD
+        self.center = center
+        self.length = length
+        self.mirror = mirror
+        self.al = al
+        self.adl = adl
+
+    def __repr__(self):
+        return repr((self.length))
+
+def change_empty_location(lens_size,sensor_size,t_array):
+
+    bpy.context.scene.camera.data.lens = lens_size
+    bpy.context.scene.camera.data.sensor_width = sensor_size
+
+    width = 2
+
+    rx = bpy.context.scene.render.resolution_x
+    ry = bpy.context.scene.render.resolution_y
+
+    height = 2 * ry / rx
+    c_distance = 2 * lens_size/sensor_size
+
+    t_array = []
+    A = bpy.data.objects["A"].location;t_array.append(A)
+    AD = bpy.data.objects["A'"].location;t_array.append(AD)
+    B = bpy.data.objects["B"].location;t_array.append(B)
+    BD = bpy.data.objects["B'"].location;t_array.append(BD)
+
+    for unit in t_array:
+        move = mathutils.Vector((0,c_distance,0))
+        cl = bpy.context.scene.camera.location
+        unit.y = (cl + move).y
+
+
+
+print("TEST")
+
+
+# lens_length = 1
+# lens_limit = 16
+# lens_step = 1
+# min = 1
+# limit = 16
+# step = 0.1
+
+lens_length = 4
+lens_limit = 5
+lens_step = 1
+
+sensor_size = 1
+
+min = 1
+limit = 16
+step = 0.1
+
+while lens_length < lens_limit:
+    camera_location = bpy.context.scene.camera.location
+
+
+
+    A = bpy.data.objects["A"].location
+    AD = bpy.data.objects["A'"].location
+    B = bpy.data.objects["B"].location
+    BD = bpy.data.objects["B'"].location
+
+    t_array = [A,B,AD,BD]
+
+    # 値を変えている部分 -----------------------------------------
+    change_empty_location(lens_length,sensor_size,t_array)
+    # ------------------------------------------------------------
+
+
+    AE = (A - camera_location).normalized()
+    ADE = (AD - camera_location).normalized()
+    BE = (B - camera_location).normalized()
+
+    BDScreen = get_screen_position(BD)
+
+
+
+    a_length = min
+    ad_length = min
+    b_length = min
+
+
+    array = []
+
+    while a_length < limit:
+        AM = camera_location + AE * a_length
+        # bpy.ops.object.empty_add(type='PLAIN_AXES', view_align=False, location=AM, layers=(
+        #    False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
+        #    False, False, False, False))
+
+        ad_length = min
+        while ad_length < limit:
+            ADM = camera_location + ADE * ad_length
+            # bpy.ops.object.empty_add(type='PLAIN_AXES', view_align=False, location=ADM, layers=(
+            #    False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
+            #    False, False, False, False))
+
+            center = (AM + ADM) / 2
+            mirror = AM - center
+
+            b_length = min
+            while b_length < limit:
+                BM = camera_location + BE * b_length
+
+                BMirror = BM.reflect(mirror)
+
+                BMirrorScreen = get_screen_position(BMirror)
+
+                distance = BMirrorScreen - BDScreen
+
+                temp = Struct(AM, ADM, BM, BMirror, center, distance.length, mirror,ad_length,a_length)
+                array.append(temp)
+
+                # print(distance.length)
+
+                b_length += step
+            # print(center)
+            # bpy.ops.object.empty_add(type='PLAIN_AXES', view_align=False, location=center, layers=(
+            #   False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
+            #   False, False, False, False))
+
+
+            ad_length += step
+        a_length += step
+
+    # Sort
+    array = sorted(array, key=lambda struct: struct.length)
+    # Display
+    print(array[0].length,lens_length,array[0].al - 1,array[0].adl - 1)
+
+    # 実際に変更を加えている部分 ----------------------------------------------------------------------------
+    # Apply location and rotation to the Center
+    bpy.data.objects["center"].location = array[0].center
+    bpy.data.objects["center"].rotation_mode = 'QUATERNION'
+    bpy.data.objects["center"].rotation_quaternion = array[0].mirror.to_track_quat('Z', 'Y')
+    # -------------------------------------------------------------------------------------------------------
+
+    lens_length += lens_step
 
 # Debug ---------------------------------------------------------------------
 debug = 0
